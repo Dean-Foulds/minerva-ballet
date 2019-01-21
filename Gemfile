e4454cb6ff95be21abef9d24188e19e44c801246
deanfoulds <deanfoulds@gmail.com>
2019-01-21 16:56:16 +0000

    Add dotenv - Protect my secret data in .env file

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 gem 'actionmailer', '~> 5.0', '>= 5.0.0.1'
 gem "letter_opener", group: :development
+gem 'dotenv-rails', groups: [:development, :test]
 group :development, :test do
   gem 'byebug', platform: :mri
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,7 @@ class UserMailer < ApplicationMailer
   def welcome(user)
     @user = user  # Instance variable => available in view
 
-    mail(to: @user.email, subject: 'Welcome to Le Wagon')
+    mail(to: @user.email, subject: 'Welcome to Minerva Ballet')
     # This will render a view in `app/views/user_mailer`!
   end
 end
