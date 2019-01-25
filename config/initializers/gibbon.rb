0cf10bcc0b4dea021f2bfd4d7314e56a763ecd06
deanfoulds <deanfoulds@gmail.com>
2019-01-25 14:36:16 +0000

    added gibbon.rb reverting as this broke app

--- config/initializers/gibbon.rb
+++ config/initializers/gibbon.rb
@@ -1,3 +1,3 @@
-Gibbon::API.api_key = ENV["MAILCHIMP_API_KEY"]
-Gibbon::API.timeout = 15
-Gibbon::API.throws_exceptions = true
+# Gibbon::API.api_key = ENV["MAILCHIMP_API_KEY"]
+# Gibbon::API.timeout = 15
+# Gibbon::API.throws_exceptions = true
