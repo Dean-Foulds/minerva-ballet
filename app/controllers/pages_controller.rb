2d27f24777b7c58e2ec37947c87348d8c8097623
deanfoulds <deanfoulds@gmail.com>
2019-01-25 14:54:46 +0000

    user edit page updated

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,14 @@
 class PagesController < ApplicationController
-    skip_before_action :authenticate_user!, only: [:index, :about, :cookie_eu]
+    skip_before_action :authenticate_user!, only: [:edit, :index, :about, :cookie_eu]
 
     def index
   
     end
 
     def about
+    end
+
+    def edit
     end
 
     def cookie_eu
