947af514b913aa1bbaaee9cb194292a01f5735a9
deanfoulds <deanfoulds@gmail.com>
2019-01-26 16:20:25 +0000

    caledar added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'pages/about', to: 'pages#about', as: :about
+  get 'pages/members', to: 'pages#members', as: :members
   get 'pages/cookie_eu', to: 'pages#cookie_eu', as: :cookie_eu
   namespace :admin do
     resources :users
