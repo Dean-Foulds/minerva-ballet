a886e441bcba775245dc30d4df216975c86a0a5b
Dean Foulds <deanfoulds@gmail.com>
2019-02-06 10:46:39 +0000

    link to four seassons added to members page

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -12,6 +12,6 @@ class PhotoUploader < CarrierWave::Uploader::Base
 
   version :bright_face do
     cloudinary_transformation effect: "brightness:15", radius: 15,
-      width: 170, height: 170, crop: :fill, gravity: :face
+      width: 150, height: 150, crop: :fill, gravity: :face
   end
 end
