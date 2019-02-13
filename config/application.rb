634dbf2a77df8ca4c385949610b2f98dccfe6be8
Dean Foulds <deanfoulds@gmail.com>
2019-02-13 10:21:05 +0000

    angle ignore yet again

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ Bundler.require(*Rails.groups)
 
 module MinervaBallet
   class Application < Rails::Application
-    config.assets.initialize_on_precompile = false
+    config.assets.initialize_on_precompile = true
     config.generators do |g|
       g.test_framework :rspec,
         fixtures: true,
