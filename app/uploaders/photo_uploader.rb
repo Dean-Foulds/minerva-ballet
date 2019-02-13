c805548e8cfbeb7decdbcbcba0759fc1628d8379
Dean Foulds <deanfoulds@gmail.com>
2019-02-13 10:07:49 +0000

    photo non rotate

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -14,6 +14,6 @@ class PhotoUploader < CarrierWave::Uploader::Base
 
   version :bright_face do
     cloudinary_transformation effect: "brightness:15", radius: 15,
-      width: 150, height: 150, crop: :fill, gravity: :face, a_angle: :ignore 
+      width: 150, height: 150, crop: :fill, gravity: :face, angle: :ignore 
   end
 end
