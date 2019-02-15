98e37f982a9275adb12a036f3e187d91b8e20231
Dean Foulds <deanfoulds@gmail.com>
2019-02-15 17:52:15 +0000

    meta data added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   mount Attachinary::Engine => "/attachinary"
-
+  match "/404", to: "errors#not_found", via: :all
+  match "/422", to: "errors#unacceptable", via: :all
+  match "/500", to: "errors#internal_server_error", via: :all
   resources :events
   get 'pages/about', to: 'pages#about', as: :about
   get 'pages/members', to: 'pages#members', as: :members
