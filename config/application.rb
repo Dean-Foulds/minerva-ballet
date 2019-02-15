ccd0622ddc46f40694b169f9429ee82ce9408ebe
Dean Foulds <deanfoulds@gmail.com>
2019-02-15 17:56:52 +0000

    meta data add

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,7 @@
 require_relative 'boot'
 require 'rails/all'
 require "attachinary/orm/active_record"
-config.exceptions_app = self.routes
+#config.exceptions_app = self.routes
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
