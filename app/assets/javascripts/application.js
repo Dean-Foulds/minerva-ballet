a6063422a2d3a4042d48709ac52086f6a92c9ee3
Dean Foulds <deanfoulds@gmail.com>
2019-10-04 13:42:59 +0100

    jquery version removed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,7 +9,6 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//= require jquery
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
