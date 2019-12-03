2f1003143fa98023b178c15326dc646d02f83dff
Dean Foulds <deanfoulds@gmail.com>
2019-12-03 17:52:24 +0000

    list changed to audience

--- app/services/subscribe_to_newsletter_service.rb
+++ app/services/subscribe_to_newsletter_service.rb
@@ -3,11 +3,11 @@ class SubscribeToNewsletterService
   def initialize(user)
     @user = user
     @gibbon = Gibbon::Request.new(api_key: ENV['MAILCHIMP_API_KEY'])
-    @list_id = ENV['MAILCHIMP_AUDIENCE_ID']
+    @audience_id = ENV['MAILCHIMP_AUDIENCE_ID']
   end
 
   def call
-    @gibbon.lists(@list_id).members.create(
+    @gibbon.lists(@audience_id).members.create(
       body: {
         email_address: @user.email,
         status: "subscribed",
